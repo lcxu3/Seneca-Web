c68a7b081e714e2029d526ef4fafb709e309e4cb
Lily Xu <84281654+lcxu3@users.noreply.github.com>
2024-12-19 17:13:34 -0500

    Add files via upload

---
+++ Assignments 5&6/src/script.js
@@ -0,0 +1,9 @@
+
+
+function displayHide() {
+    document.getElementById("form-group").style.visibility = "hidden";
+ }
+ function displayShow() {
+    document.getElementById("form-group").style.visibility = "visible";
+ }
+
